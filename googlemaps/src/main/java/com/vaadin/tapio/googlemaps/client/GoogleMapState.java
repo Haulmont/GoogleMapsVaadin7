8e5fbf2f2cd3b684a2de0fbe9752fd7f78e0d192
Roman Pinyazhin <pinyazhin@haulmont.com>
2019-02-26 14:47:27 +0400

    Update google map api version to v3.35 cuba-platform/charts#73

--- googlemaps/src/main/java/com/vaadin/tapio/googlemaps/client/GoogleMapState.java
+++ googlemaps/src/main/java/com/vaadin/tapio/googlemaps/client/GoogleMapState.java
@@ -83,5 +83,5 @@ public class GoogleMapState extends AbstractComponentState {
     public Map<Long, GoogleMapLabel> labels = new HashMap<Long, GoogleMapLabel>();
 
     public String apiUrl = null;
-    public String mapsApiVersion = "3.28";
+    public String mapsApiVersion = "3.35";
 }
